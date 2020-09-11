2eaeb7bf5525b452497c3ceda7635535f93c95b6
Murat <68085389+muratj@users.noreply.github.com>
2020-09-10 20:39:15 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,27 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-var hour = new Date().getHours();
-
-var greetingStyle = {
-  color: ""
-};
-
-var dayTime;
-if (hour < 12) {
-  dayTime = "Morning";
-  greetingStyle.color = "green";
-} else if (hour < 18) {
-  dayTime = "Afternoon";
-  greetingStyle.color = "red";
-} else {
-  dayTime = "Night";
-  greetingStyle.color = "blue";
-}
-console.log(hour);
-ReactDOM.render(
-  <h1 className="greeting" style={greetingStyle}>
-    Good {dayTime}
-  </h1>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
